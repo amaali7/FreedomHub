66202fef763c7a88f7d3a96d54ebae7b51564e71
Abdallah Adam <amaali1991@gmail.com>
2020-12-31 19:45:36 +0200

    fix apped text function with | without root accrss

--- FreedomHub/ipTables/Iptaples.py
+++ FreedomHub/ipTables/Iptaples.py
@@ -1,40 +1,48 @@
 import os
-from subprocess import run
+from subprocess import run, DEVNULL
 
 
 class FileWithRoot():
 
-    def __init__(self, fileName, path):
-        
+    """
+        This class for write file with or without root access
+        depend on the state 
+    """
+
+    def __init__(self, fileName, path, rootAccess=False):
         self.name = fileName
         self.path = path
-        self.existanc = self.__exist__()
-
-    def __exist__(self):
-        exist = False
-        for root, dirs, files in os.walk(self.path):
-            if self.name in files:
-                exist = True
-        return exist
+        self.rootNeed = rootAccess
 
     def appendLines(self, text):
-        text.replace('\\n', '\n').replace('\\t', '\t')
-        cmd = "sudo \"echo -e "+ text+"\" >> "+self.path+self.name
-        print(cmd)
+
+        coolText = text.replace('\n', "\\n")
+        if self.rootNeed:
+            command = "echo \"" + coolText+"\" | sudo tee -a "+self.path+self.name
+            instruction =  run(command, shell=True, text=True, capture_output=DEVNULL)
         
-        if self.existanc:
-            instruction =  run(cmd, shell=True, text=True, capture_output=True)
-            print(instruction.returncode)
         else:
-            run("sudo touch "+self.path+self.name, shell=True, text=True, capture_output=True)
-            instruction =  run(cmd, shell=True, text=True, capture_output=True)
-            print(instruction.returncode)
+            command = "echo \"" + coolText+"\" | tee -a "+self.path+self.name
+            instruction =  run(command, shell=True, text=True, capture_output=DEVNULL)
+        
+        if instruction.returncode == 0:
+            return True
+        else:
+            return False
         
 
-    def overwrite(self):
+    def overwrite(self, text):
         
-        pass
-
-rule = FileWithRoot('root.txt','/')
-
-rule.appendLines("jlskfjakljakfjlksjfaksjdlkfljasdfjaslkjflks")
+        coolText = text.replace('\n', "\\n")
+        if self.rootNeed:
+            command = "echo \"" + coolText+"\" | sudo tee -a "+self.path+self.name
+            instruction =  run(command, shell=True, text=True, capture_output=DEVNULL)
+        
+        else:
+            command = "echo \"" + coolText+"\" | tee -a "+self.path+self.name
+            instruction =  run(command, shell=True, text=True, capture_output=DEVNULL)
+        
+        if instruction.returncode == 0:
+            return True
+        else:
+            return False
