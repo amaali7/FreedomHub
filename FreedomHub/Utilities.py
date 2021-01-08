8ec6b8eb5bcdbc886d9523412268ee73c54bfaa9
Abdallah Adam <amaali1991@gmail.com>
2021-01-08 16:27:31 +0200

    Web State Start Now

--- FreedomHub/Utilities.py
+++ FreedomHub/Utilities.py
@@ -1,8 +1,9 @@
 import os
 from subprocess import run, DEVNULL
+import re
 
 
-class FileWithRootA():
+class FileWithRoot():
 
     """
         This class for write file with or without root access
@@ -13,43 +14,87 @@ class FileWithRootA():
         self.name = fileName
         self.path = path
         self.rootNeed = rootAccess
+        self.isExist = self.__isExist__()
+
+    def __isExist__(self):
+        return os.path.isfile(self.path+self.name)
+
+    def readFile(self):
+        if self.isExist:
+            if self.rootNeed:
+                command = "sudo cat "+self.path+self.name
+                instruction =  run(command, shell=True, text=True, capture_output=True)
+                if instruction.returncode == 0:
+                    return instruction.stdout
+                else:
+                    return None
+            else:
+                return open(self.path+self.name, 'r').read()
+        else:
+            print("No such file")
+        
+        
+        
+    def modefyFile(self, pattern, replacement):
+        if self.isExist:
+            text = self.readFile()
+            if text != None:
+                for i in len(pattern):
+                    changedStrings = re.sub(pattern[i],replacement[i],text)
+                    text = changedStrings
+
+                if self.overwrite(text):
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            print("No such file")
+                
+
 
     def appendLines(self, text):
-
-        coolText = text.replace('\n', "\\n")
-        if self.rootNeed:
-            command = "echo \"" + coolText+"\" | sudo tee -a "+self.path+self.name
-            instruction =  run(command, shell=True, text=True, capture_output=DEVNULL)
-        
+        if self.isExist:
+            coolText = text.replace('\n', "\\n")
+            if self.rootNeed:
+                command = "echo \"" + coolText+"\" | sudo tee -a "+self.path+self.name
+                instruction =  run(command, shell=True, text=True, capture_output=DEVNULL)
+            else:
+                command = "echo \"" + coolText+"\" | tee -a "+self.path+self.name
+                instruction =  run(command, shell=True, text=True, capture_output=DEVNULL)
+            if instruction.returncode == 0:
+                return True
+            else:
+                return False
+            
         else:
-            command = "echo \"" + coolText+"\" | tee -a "+self.path+self.name
-            instruction =  run(command, shell=True, text=True, capture_output=DEVNULL)
-        
-        if instruction.returncode == 0:
-            return True
-        else:
-            return False
+            print('No such file')
         
 
     def overwrite(self, text):
-        
-        coolText = text.replace('\n', "\\n")
-        if self.rootNeed:
-            move = "sudo mv "+self.path+self.name +" "+self.path+self.name+'.backup'
-            command = "echo \"" + coolText+"\" | sudo tee -a "+self.path+self.name
-            instruction_m =  run(move, shell=True, text=True, capture_output=DEVNULL)
-            instruction_c =  run(command, shell=True, text=True, capture_output=DEVNULL)
-        
+        if self.isExist:
+            coolText = text.replace('\n', "\\n")
+            if self.rootNeed:
+                move = "sudo mv "+self.path+self.name +" "+self.path+self.name+'.backup'
+                command = "echo \"" + coolText+"\" > "+self.path+self.name
+                instruction_m =  run(move, shell=True, text=True, capture_output=DEVNULL)
+                instruction_c =  run(command, shell=True, text=True, capture_output=DEVNULL)
+            
+            else:
+                move = "mv "+self.path+self.name +" "+self.path+self.name+'.backup'
+                command = "sudo echo \"" + coolText+"\" > "+self.path+self.name
+                instruction_m =  run(move, shell=True, text=True, capture_output=DEVNULL)
+                instruction_c =  run(command, shell=True, text=True, capture_output=DEVNULL)
+            
+            if instruction_c.returncode == 0 and instruction_m.returncode == 0:
+                return True
+            else:
+                return False
+
         else:
-            move = "mv "+self.path+self.name +" "+self.path+self.name+'.backup'
-            command = "echo \"" + coolText+"\" | sudo tee -a "+self.path+self.name
-            instruction_m =  run(move, shell=True, text=True, capture_output=DEVNULL)
-            instruction_c =  run(command, shell=True, text=True, capture_output=DEVNULL)
+            print("No such file")
         
-        if instruction_c.returncode == 0 and instruction_m.returncode == 0:
-            return True
-        else:
-            return False
 
 
 class DebianPackage():
--- FreedomHub/wifiAccessControll.py
+++ FreedomHub/wifiAccessControll.py
@@ -1,14 +1,23 @@
 from FreedomHub.SystemD import SystemDUnit
-
+from FreedomHub.Utilities import FileWithRoot
+import re
 class WifiAP():
-    
+    """
+        Check your wireless dongle supports AP mode (Access Point)
+    """
     hostapd = SystemDUnit('hostapd')
     isc_dhcp_server = SystemDUnit('isc-dhcp-server')
 
-    def __init__(self, interface="wlan0"):
+    def __init__(self, interface="wlan0", accept_mac_file='/etc/hostapd/hostapd.accept', deny_mac_file='/etc/hostapd/hostapd.deny'):
+    
         self.packageStatus = self.__checkPakages__()
         self.softwareRequirementsState = all(value == True for value in self.packageStatus.values())
         self.interface= interface
+        self.hostapd_cofig = self.__hostapd_config__()
+        self.accept_mac_file = accept_mac_file
+        self.deny_mac_file = deny_mac_file
+
+
 
     def __checkPakages__(self):
         hostapd_state = False
@@ -26,22 +35,82 @@ class WifiAP():
             print("dnsmasq not install!")
         return {'hostapd':hostapd_state, 'isc-dhcp-server':isc_dhcp_server_state}
 
+
+    def __getIp__(self,ip):
+        pattern = re.compile(r'^\d*\.\d*\.\d*\.\d*$')
+        match = pattern.search(ip)
+        if match:
+            return match.string
+        else:
+            return None
+        
+    def __hostapd_config__(self):
+        hostapd_config = FileWithRoot('hostapd.conf','/etc/hostapd/',True)
+        acept_config = FileWithRoot('hostapd.conf','/etc/hostapd/',True)
+        denay_config = FileWithRoot('hostapd.conf','/etc/hostapd/',True)
+        return hostapd_config.readFile
+
     def refresh_services(self):
         if self.hostapd.reload() and self.isc_dhcp_server.reload():
             return True
         else:
             return False
 
+    def setInterfaceIp(self,ipAddress,netmask):
+        path ='/etc/network/'
+        name = 'interfaces'
+        ins = FileWithRoot(name, path, True)
+        # Remove the dhcp entry 
+        ins.modefyFile(['iface default inet dhcp'],['#iface default inet dhcp'])
+        ip = self.__getIp__(ipAddress)
+        net = self.__getIp__(netmask)
+        # Configures a static IP address. 
+        if ip and net:
+            conf = "iface wlan0 inet static"+'\n\t'+"address "+ip+'\n\t'+"netmask "+net
+            
+            if ins.appendLines(conf):
+                return True
+            else:
+                return False
+        else:
+            return False
+        
+
+
 
     def isc_dhcp_server_conf(self, subnet="10.5.5.0", netmask="255.255.255.0", range_start="10.5.5.100",
                              range_end="10.5.5.254", routers="10.5.5.1", broadcast_address="10.5.5.255",
                               default_lease_time="600", max_lease_time="7200"):
-
+        # /etc/dhcp/dhcpd.conf
+        name = 'dhcpd.conf'
+        path = '/etc/dhcp/'
+        ins = FileWithRoot(name, path, True)
+        # Remove the dhcp entry and configure internal subnet
+        ins.modefyFile(['option domain-name','option domain-name-servers','#authoritative'],
+                        ['#option domain-name','#option domain-name-servers','authoritative'])
+        
         conf =("subnet "+subnet+" netmask "+netmask+" {"+'\n\t'+"range "+range_start+" "+range_end+";"+'\n\t'
         +"option routers "+routers+";"+'\n\t'+"option broadcast-address "+broadcast_address+";"+'\n\t'+
         "default-lease-time "+default_lease_time+";"+'\n\t'+"max-lease-time "+max_lease_time+";"+'\n\t'+"}")
+        main_conf = ins.appendLines(conf)   
+        # /etc/default/isc-dhcp-server
+        isc = FileWithRoot('isc-dhcp-server','/etc/default/',True)
+        isc.modefyFile(r'INTERFACES=\".\"','INTERFACES=\"'+self.interface+'\"')
 
-        # conf = 'interface ' + self.interface+'\n'+'\tstatic ip_address='+self.serverIP+'\n'+"\tnohook wpa_supplicant"
-        # dhcpcd_conf_file = FileWithRoot('dhcpd.conf','/etc/dhcp/dhcpd.conf',True)
-        # dhcpcd_conf_file.appendLines(conf)   
-        print(conf)
+        if main_conf:
+            return True
+        else:
+            return False
+
+
+    def confgure_hostapd(self,ssid='FreedomHub',password='freedomhub',
+            country_code='SD',hw_mode='g',channel='0', macaddr_acl='1',
+            wpa='2',wpa_key_mgmt='WPA-PSK',wpa_pairwise='TKIP', rsn_pairwise='CCMP',
+            extra_conf=[],extra_conf_value=[]):
+        
+        conf = f'interface={self.interface}\ndriver=hostap\nssid={ssid}\ncountry_code={country_code}\nhw_mode={hw_mode}\nchannel={channel}\nmacaddr_acl={macaddr_acl}\ndeny_mac_file={self.deny_mac_file}\nauth_algs=1\nwpa={wpa}\nwpa_passphrase={password}\nwpa_key_mgmt={wpa_key_mgmt}\nwpa_pairwise={wpa_pairwise}\nrsn_pairwise={rsn_pairwise}'
+        if len(extra_conf)>0:
+            for con,val in zip(extra_conf,extra_conf_value):
+                conf+=f'\n{con}={val}'
+
+        print( conf)
